95473f950000131ad88a3c6e64e9f00ef8196666
tegmen88 <91795901+tegmen88@users.noreply.github.com>
2023-11-27 11:06:37 +0100

    init cihan

---
+++ src/main/java/com/group4/group4/ControllerT.java
@@ -0,0 +1,13 @@
+package com.group4.group4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerT {
+    // Home page
+    @GetMapping("/home")
+    String getHome(){
+        return "home";
+    }
+}
