d88640c04c1bde4a665fbabfa129248eb810062b
dennis <isacsson_89@hotmail.com>
2023-11-27 10:23:52 +0100

    SpringBoot first commit

---
+++ src/main/java/com/group4/group4/Group4Application.java
@@ -0,0 +1,13 @@
+package com.group4.group4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/group4/group4/Group4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group4.group4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
