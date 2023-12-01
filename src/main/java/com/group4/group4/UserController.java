f8a14fc637fae478bcc61e94c4ee7d44c88e640f
tegmen88 <91795901+tegmen88@users.noreply.github.com>
2023-12-01 11:49:51 +0100

    init

--- src/main/java/com/group4/group4/UserController.java
+++ src/main/java/com/group4/group4/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,18 +15,21 @@ import org.springframework.ui.Model;
 public class UserController {
 
     private List<User> userList = new ArrayList<>();
+    private List<User> userDetails = new ArrayList<>();
 
     @GetMapping("/addUser")
     public String addUserForm(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("userList", userList);
         return "addUserForm";
     }
 
-
-
     @PostMapping("/addUser")
-    public String addUser(@ModelAttribute("user") User user) {
+    public String addUser(@ModelAttribute("user") User user, Model model) {
         userList.add(user);
-        return "redirect:/";
+        model.addAttribute("userList", userList);
+        return "redirect:/addUser";
     }
+
+
 }
