ae0d12a210f89c8ae6ca602dbb0d598a5d847d11
Roberto Tencic <roberto.tencic@outlook.com>
2023-12-06 20:43:15 +0100

    done-button

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,24 +1,29 @@
-console.log("Cihan testar");
-
 // Dagens datum.
 let todayDate = new Date();
 // Gårdagens datum.
 let yesterdayDate = new Date();
 yesterdayDate.setDate(todayDate.getDate() - 1);
-yesterdayDate = yesterdayDate.toLocaleDateString();
-todayDate = todayDate.toLocaleDateString();
 // Input-fältet där man anger deadline.
 let deadlineInput = document.getElementById("deadline");
-// Vi kommer bara kunna välja dagens datum eller senare som deadline.
-deadlineInput.setAttribute("min", yesterdayDate);
+deadlineInput.setAttribute("min", yesterdayDate.toLocaleDateString());
 
-// Vi hämtar varje task.
-let tasks = document.getElementsByClassName("task");
 // Loopar igenom varje task och kontrollerar deadline.
+let tasks = document.getElementsByClassName("task-description");
 for (let i = 0; i < tasks.length; i++) {
     let deadlineDate = new Date(tasks.item(i).innerHTML.slice(tasks.item(i).innerHTML.length - 10, tasks.item(i).innerHTML.length)).toLocaleDateString();
-    // Är deadlinen passerad? Då blir texten röd.
-    if (deadlineDate < todayDate) {
+    if (deadlineDate < todayDate.toLocaleDateString()) {
         tasks.item(i).style.color = "red";
     }
+}
+
+// Loopar igenom varje knapp och lägger till en eventhanterare.
+let doneButtons = document.getElementsByClassName("done-button");
+for (let i = 0; i < doneButtons.length; i++) {
+    doneButtons.item(i).addEventListener("change", (e) => {
+        if (e.target.checked == true) {
+            tasks.item(i).style.textDecoration = "line-through";
+        } else {
+            tasks.item(i).style.textDecoration = "none";
+        }
+    });
 }
