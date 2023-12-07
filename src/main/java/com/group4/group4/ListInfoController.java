9f6ac464e31740c5495a071b50cb97fc7befbf89
ChawanShasawar <chawan.shasawar@hotmail.se>
2023-12-07 15:37:58 +0100

    sista

--- src/main/java/com/group4/group4/ListInfoController.java
+++ src/main/java/com/group4/group4/ListInfoController.java
@@ -1,11 +1,9 @@
 package com.group4.group4;
 
 import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
